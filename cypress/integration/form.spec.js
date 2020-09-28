290cd6a62193be1973388be588c1e95f71c90bef
codacy20 <kmuc72@gmail.com>
2020-09-28 22:42:30 +0200

    input test cases

--- cypress/integration/form.spec.js
+++ cypress/integration/form.spec.js
@@ -2,5 +2,8 @@ describe("Form test", () => {
     it("Can fill the form", () => {
         cy.visit("/");
         cy.get("form");
+        cy.get('input[name="name"]')
+        .type("Molly")
+        .should("have.value", "Molly");
     });
 });
